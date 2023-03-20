d163ddb322e0516716e9e4e8258af3ca33d20e8a
sszz-w <1502367824@qq.com>
2023-03-20 12:20:07 +0800

    test

--- pat/src/test.cpp
+++ pat/src/test.cpp
@@ -1,11 +1,9 @@
-#include <bits/stdc++.h>
+#include <iostream>
+
 using namespace std;
-int main(){
-    vector<int> a={1,2,3};
-    auto it=a.begin();
-    a.erase(it);
-    for(int i=0;i<a.size();i++){
-        cout<<a[i]<<endl;
-    }
+
+int main()
+{
+    cout << "Hello World!" << endl;
     return 0;
 }
