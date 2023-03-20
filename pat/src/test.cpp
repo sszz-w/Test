7e5ad12268c27e4f46b62b46f86d5501dbaa8b5f
sszz-w <1502367824@qq.com>
2023-03-20 13:14:28 +0800

    upgrade

--- pat/src/test.cpp
+++ pat/src/test.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    vector<string> msg{"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    
     return 0;
 }
